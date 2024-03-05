3b6779ede283e01a7b9aab769a3de54c22bb7f90
Adam Reeve <adam.reeve@gr-oss.io>
2024-03-05 21:46:18 +1300

    Initial HivePartitioning implementation

---
+++ ParquetSharp.Dataset.Test/Partitioning/TestHivePartitioning.cs
@@ -0,0 +1,166 @@
+using Apache.Arrow;
+using Apache.Arrow.Types;
+using NUnit.Framework;
+using ParquetSharp.Dataset.Partitioning;
+
+namespace ParquetSharp.Dataset.Test.Partitioning;
+
+public class TestHivePartitioning
+{
+    [Test]
+    public void TestEmptySchema()
+    {
+        var partitioningSchema = new Apache.Arrow.Schema.Builder().Build();
+        var partitioning = new HivePartitioning(partitioningSchema);
+
+        var subsetData = partitioning.Parse(System.Array.Empty<string>());
+
+        Assert.That(subsetData.Length, Is.EqualTo(1));
+        Assert.That(subsetData.Schema.FieldsList.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void TestSingleIntPartitionField()
+    {
+        var partitioningSchema = new Apache.Arrow.Schema.Builder()
+            .Field(new Field("x", new Int64Type(), nullable: true))
+            .Build();
+        var partitioning = new HivePartitioning(partitioningSchema);
+
+        var subsetData = partitioning.Parse(new [] {"x=3"});
+
+        Assert.That(subsetData.Length, Is.EqualTo(1));
+        Assert.That(subsetData.Schema.FieldsList.Count, Is.EqualTo(1));
+        var column = subsetData.Column("x") as Int64Array;
+        Assert.That(column, Is.Not.Null);
+        Assert.That(column!.GetValue(0), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void TestSingleStringPartitionField()
+    {
+        var partitioningSchema = new Apache.Arrow.Schema.Builder()
+            .Field(new Field("x", new StringType(), nullable: true))
+            .Build();
+        var partitioning = new HivePartitioning(partitioningSchema);
+
+        // Should respect type even if this value can be interpreted as int
+        var subsetData = partitioning.Parse(new [] {"x=3"});
+
+        Assert.That(subsetData.Length, Is.EqualTo(1));
+        Assert.That(subsetData.Schema.FieldsList.Count, Is.EqualTo(1));
+        var column = subsetData.Column("x") as StringArray;
+        Assert.That(column, Is.Not.Null);
+        Assert.That(column!.GetString(0), Is.EqualTo("3"));
+    }
+
+    [Test]
+    public void TestMultiplePartitionField()
+    {
+        var partitioningSchema = new Apache.Arrow.Schema.Builder()
+            .Field(new Field("x", new Int64Type(), nullable: true))
+            .Field(new Field("y", new StringType(), nullable: true))
+            .Build();
+        var partitioning = new HivePartitioning(partitioningSchema);
+
+        // Should respect type even if this value can be interpreted as int
+        var subsetData = partitioning.Parse(new [] {"y=hello", "x=4"});
+
+        Assert.That(subsetData.Length, Is.EqualTo(1));
+        Assert.That(subsetData.Schema.FieldsList.Count, Is.EqualTo(2));
+
+        var xColumn = subsetData.Column("x") as Int64Array;
+        Assert.That(xColumn, Is.Not.Null);
+        Assert.That(xColumn!.GetValue(0), Is.EqualTo(4));
+
+        var yColumn = subsetData.Column("y") as StringArray;
+        Assert.That(yColumn, Is.Not.Null);
+        Assert.That(yColumn!.GetString(0), Is.EqualTo("hello"));
+    }
+
+    [Test]
+    public void TestNullValue()
+    {
+        var partitioningSchema = new Apache.Arrow.Schema.Builder()
+            .Field(new Field("x", new Int64Type(), nullable: true))
+            .Build();
+        var partitioning = new HivePartitioning(partitioningSchema);
+
+        var subsetData = partitioning.Parse(new [] {"x=__HIVE_DEFAULT_PARTITION__"});
+
+        Assert.That(subsetData.Length, Is.EqualTo(1));
+        Assert.That(subsetData.Schema.FieldsList.Count, Is.EqualTo(1));
+        var column = subsetData.Column("x") as Int64Array;
+        Assert.That(column, Is.Not.Null);
+        Assert.That(column!.GetValue(0), Is.Null);
+    }
+
+    [Test]
+    public void TestNullValueForNonNullableField()
+    {
+        var partitioningSchema = new Apache.Arrow.Schema.Builder()
+            .Field(new Field("x", new Int64Type(), nullable: false))
+            .Build();
+        var partitioning = new HivePartitioning(partitioningSchema);
+
+        var exception = Assert.Throws<ArgumentException>(
+            () => partitioning.Parse(new[] {"x=__HIVE_DEFAULT_PARTITION__"}));
+        Assert.That(exception!.ParamName, Is.EqualTo("pathComponents"));
+    }
+
+    [Test]
+    public void TestUnexpectedFieldWithEmptySchema()
+    {
+        var partitioningSchema = new Apache.Arrow.Schema.Builder().Build();
+        var partitioning = new HivePartitioning(partitioningSchema);
+
+        var exception = Assert.Throws<ArgumentException>(
+            () => partitioning.Parse(new[] {"column=value"}));
+        Assert.That(exception!.ParamName, Is.EqualTo("pathComponents"));
+    }
+
+    [Test]
+    public void TestSplitOnInvalidFieldName()
+    {
+        var partitioningSchema = new Apache.Arrow.Schema.Builder()
+            .Field(new Field("x", new Int64Type(), nullable: true))
+            .Build();
+        var partitioning = new HivePartitioning(partitioningSchema);
+
+        var exception = Assert.Throws<ArgumentException>(
+            () => partitioning.Parse(new[] {"y=3"}));
+        Assert.That(exception!.ParamName, Is.EqualTo("pathComponents"));
+        Assert.That(exception.Message, Does.Contain("'y'"));
+    }
+
+    [Test]
+    public void TestInvalidSyntax()
+    {
+        var partitioningSchema = new Apache.Arrow.Schema.Builder()
+            .Field(new Field("x", new Int64Type(), nullable: true))
+            .Build();
+        var partitioning = new HivePartitioning(partitioningSchema);
+
+        var exception = Assert.Throws<ArgumentException>(
+            () => partitioning.Parse(new[] {"x3"}));
+        Assert.That(exception!.ParamName, Is.EqualTo("pathComponents"));
+        Assert.That(exception.Message, Does.Contain("'x3'"));
+    }
+
+    [Test]
+    public void TestUrlEncodedValues()
+    {
+        var partitioningSchema = new Apache.Arrow.Schema.Builder()
+            .Field(new Field("x =#!/", new StringType(), nullable: true))
+            .Build();
+        var partitioning = new HivePartitioning(partitioningSchema);
+
+        var subsetData = partitioning.Parse(new [] {"x%20%3d%23%21%2f=%3a%3b%2b%27%22"});
+
+        Assert.That(subsetData.Length, Is.EqualTo(1));
+        Assert.That(subsetData.Schema.FieldsList.Count, Is.EqualTo(1));
+        var column = subsetData.Column("x =#!/") as StringArray;
+        Assert.That(column, Is.Not.Null);
+        Assert.That(column!.GetString(0), Is.EqualTo(":;+'\""));
+    }
+}
---
+++ ParquetSharp.Dataset/Partitioning/HivePartitioning.cs
@@ -0,0 +1,66 @@
+using System.Web;
+using Apache.Arrow;
+
+namespace ParquetSharp.Dataset.Partitioning;
+
+public sealed class HivePartitioning : IPartitioning
+{
+    public HivePartitioning(Apache.Arrow.Schema schema)
+    {
+        Schema = schema ?? throw new ArgumentNullException(nameof(schema));
+    }
+
+    public Apache.Arrow.Schema Schema { get; }
+
+    public RecordBatch Parse(string[] pathComponents)
+    {
+        var arrays = new List<IArrowArray>();
+        var fields = new List<Field>();
+
+        foreach (var component in pathComponents)
+        {
+            var split = component.Split('=', 2);
+            if (split.Length != 2)
+            {
+                throw new ArgumentException(
+                    $"Invalid directory name for Hive partitioning '{component}'", nameof(pathComponents));
+            }
+
+            var fieldName = HttpUtility.UrlDecode(split[0]);
+            var fieldValue = HttpUtility.UrlDecode(split[1]);
+            var field = Schema.GetFieldByName(fieldName);
+            if (field == null)
+            {
+                throw new ArgumentException(
+                    $"Invalid field name '{fieldName}' for partitioning", nameof(pathComponents));
+            }
+
+            if (fieldValue == HiveNullValueFallback)
+            {
+                if (!field.IsNullable)
+                {
+                    throw new ArgumentException(
+                        $"Found null value for non-nullable partition field '{fieldName}'", nameof(pathComponents));
+                }
+                fieldValue = null;
+            }
+
+            var parser = new ScalarParser(fieldValue);
+            field.DataType.Accept(parser);
+            var array = parser.ScalarArray!;
+
+            fields.Add(field);
+            arrays.Add(array);
+        }
+
+        var schemaBuilder = new Apache.Arrow.Schema.Builder();
+        foreach (var field in fields)
+        {
+            schemaBuilder.Field(field);
+        }
+
+        return new RecordBatch(schemaBuilder.Build(), arrays, 1);
+    }
+
+    private const string HiveNullValueFallback = "__HIVE_DEFAULT_PARTITION__";
+}
