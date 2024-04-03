089e5c3843731d48e743481e016eea374a2a80a7
Adam Reeve <adam.reeve@gr-oss.io>
2024-04-03 14:47:45 +1300

    More thorough integer filter tests

--- ParquetSharp.Dataset.Test/Filter/TestIntFilter.cs
+++ ParquetSharp.Dataset.Test/Filter/TestIntFilter.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Apache.Arrow;
 using NUnit.Framework;
 
@@ -6,176 +8,213 @@ namespace ParquetSharp.Dataset.Test.Filter;
 [TestFixture]
 public class TestIntFilter
 {
-    [Test]
-    public void TestIntValueFilter()
+    [DatapointSource]
+    private static readonly long[] LongValues =
     {
-        var filter = Col.Named("x").IsEqualTo(2);
+        long.MinValue, long.MinValue + 1L,
+        int.MinValue - 1L, int.MinValue, int.MinValue + 1L,
+        short.MinValue - 1L, short.MinValue, short.MinValue + 1L,
+        sbyte.MinValue - 1L, sbyte.MinValue, sbyte.MinValue + 1L,
+        -3, -2, -1, 0, 1, 2, 3,
+        sbyte.MaxValue - 1L, sbyte.MaxValue, sbyte.MaxValue + 1L,
+        byte.MaxValue - 1L, byte.MaxValue, byte.MaxValue + 1L,
+        short.MaxValue - 1L, short.MaxValue, short.MaxValue + 1L,
+        ushort.MaxValue - 1L, ushort.MaxValue, ushort.MaxValue + 1L,
+        int.MaxValue - 1L, int.MaxValue, int.MaxValue + 1L,
+        uint.MaxValue - 1L, uint.MaxValue, uint.MaxValue + 1L,
+        long.MaxValue - 1L, long.MaxValue,
+    };
 
-        var zeroFilter = Col.Named("x").IsEqualTo(0);
-        var negativeValueFilter = Col.Named("x").IsEqualTo(-2);
-        var maxInt64Filter = Col.Named("x").IsEqualTo(long.MaxValue);
+    [DatapointSource]
+    private static readonly (long, long)[] FilterRanges = LongValues
+        .SelectMany(rangeStart => LongValues.Select(rangeEnd => (rangeStart, rangeEnd)))
+        .Where(range => range.rangeEnd >= range.rangeStart)
+        .Concat(new (long, long)[] { (2, 1) })
+        .ToArray();
 
-        foreach (var (value, expected) in new (long?, bool)[]
-                 {
-                     (2, true),
-                     (1, false),
-                     (null, false),
-                 })
-        {
-            TestIntColumnFilterWithAllTypes(filter, value, expected);
-        }
+    private static readonly int[] IntValues =
+    {
+        int.MinValue, int.MinValue + 1,
+        short.MinValue - 1, short.MinValue, short.MinValue + 1,
+        sbyte.MinValue - 1, sbyte.MinValue, sbyte.MinValue + 1,
+        -3, -2, -1, 0, 1, 2, 3,
+        sbyte.MaxValue - 1, sbyte.MaxValue, sbyte.MaxValue + 1,
+        byte.MaxValue - 1, byte.MaxValue, byte.MaxValue + 1,
+        short.MaxValue - 1, short.MaxValue, short.MaxValue + 1,
+        ushort.MaxValue - 1, ushort.MaxValue, ushort.MaxValue + 1,
+        int.MaxValue - 1, int.MaxValue,
+    };
 
-        foreach (var (value, expected) in new (long?, bool)[]
-                 {
-                     (2, false),
-                     (0, true),
-                     (null, false),
-                 })
-        {
-            TestIntColumnFilterWithAllTypes(zeroFilter, value, expected);
-        }
+    private static readonly short[] ShortValues =
+    {
+        short.MinValue, short.MinValue + 1,
+        sbyte.MinValue - 1, sbyte.MinValue, sbyte.MinValue + 1,
+        -3, -2, -1, 0, 1, 2, 3,
+        sbyte.MaxValue - 1, sbyte.MaxValue, sbyte.MaxValue + 1,
+        byte.MaxValue - 1, byte.MaxValue, byte.MaxValue + 1,
+        short.MaxValue - 1, short.MaxValue,
+    };
 
-        TestIntColumnFilterWithAllTypes(negativeValueFilter, 2, false);
-        TestIntColumnFilterWithAllTypes(maxInt64Filter, 2, false);
+    private static readonly sbyte[] SByteValues =
+    {
+        sbyte.MinValue, sbyte.MinValue + 1,
+        -3, -2, -1, 0, 1, 2, 3,
+        sbyte.MaxValue - 1, sbyte.MaxValue,
+    };
 
-        TestUInt64ColumnFilter(maxInt64Filter, long.MaxValue, true);
-        TestUInt64ColumnFilter(maxInt64Filter, ulong.MaxValue, false);
-        TestInt8ColumnFilter(negativeValueFilter, -2, true);
-        TestInt16ColumnFilter(negativeValueFilter, -2, true);
-        TestInt32ColumnFilter(negativeValueFilter, -2, true);
-        TestInt64ColumnFilter(negativeValueFilter, -2, true);
-        TestInt64ColumnFilter(maxInt64Filter, long.MaxValue, true);
+    private static readonly ulong[] ULongValues =
+    {
+        0, 1, 2, 3,
+        (ulong)sbyte.MaxValue - 1UL, (ulong)sbyte.MaxValue, (ulong)sbyte.MaxValue + 1UL,
+        byte.MaxValue - 1UL, byte.MaxValue, byte.MaxValue + 1UL,
+        (ulong)short.MaxValue - 1UL, (ulong)short.MaxValue, (ulong)short.MaxValue + 1UL,
+        ushort.MaxValue - 1UL, ushort.MaxValue, ushort.MaxValue + 1UL,
+        int.MaxValue - 1UL, int.MaxValue, int.MaxValue + 1UL,
+        uint.MaxValue - 1UL, uint.MaxValue, uint.MaxValue + 1UL,
+        long.MaxValue - 1UL, long.MaxValue, long.MaxValue + 1UL,
+        ulong.MaxValue - 1UL, ulong.MaxValue,
+    };
+
+    private static readonly uint[] UIntValues =
+    {
+        0, 1, 2, 3,
+        (uint)sbyte.MaxValue, sbyte.MaxValue + 1,
+        byte.MaxValue - 1, byte.MaxValue, byte.MaxValue + 1,
+        short.MaxValue - 1, (uint)short.MaxValue, short.MaxValue + 1,
+        ushort.MaxValue - 1, ushort.MaxValue, ushort.MaxValue + 1,
+        int.MaxValue - 1, int.MaxValue, int.MaxValue + 1u,
+        uint.MaxValue - 1, uint.MaxValue,
+    };
+
+    private static readonly ushort[] UShortValues =
+    {
+        0, 1, 2, 3,
+        sbyte.MaxValue - 1, (ushort)sbyte.MaxValue, (ushort)sbyte.MaxValue + 1,
+        byte.MaxValue - 1, byte.MaxValue, byte.MaxValue + 1,
+        short.MaxValue - 1, (ushort)short.MaxValue,
+        ushort.MaxValue - 1, ushort.MaxValue,
+    };
+
+    private static readonly byte[] ByteValues =
+    {
+        0, 1, 2, 3,
+        byte.MaxValue - 1, byte.MaxValue
+    };
+
+    [Theory]
+    public void TestComputeIntEqualityMask(long filterValue)
+    {
+        TestComputeIntEqualityMask<sbyte, Int8Array, Int8Array.Builder>(filterValue, SByteValues, val => val);
+        TestComputeIntEqualityMask<short, Int16Array, Int16Array.Builder>(filterValue, ShortValues, val => val);
+        TestComputeIntEqualityMask<int, Int32Array, Int32Array.Builder>(filterValue, IntValues, val => val);
+        TestComputeIntEqualityMask<long, Int64Array, Int64Array.Builder>(filterValue, LongValues, val => val);
+        TestComputeIntEqualityMask<byte, UInt8Array, UInt8Array.Builder>(filterValue, ByteValues, val => val);
+        TestComputeIntEqualityMask<ushort, UInt16Array, UInt16Array.Builder>(filterValue, UShortValues, val => val);
+        TestComputeIntEqualityMask<uint, UInt32Array, UInt32Array.Builder>(filterValue, UIntValues, val => val);
+        TestComputeIntEqualityMask<ulong, UInt64Array, UInt64Array.Builder>(filterValue, ULongValues, val => checked((long)val));
     }
 
-    [Test]
-    public void TestIntRangeFilter()
+    [Theory]
+    public void TestComputeIntRangeMask((long, long) filterRange)
     {
-        var filter = Col.Named("x").IsInRange(2, 5);
+        var (rangeStart, rangeEnd) = filterRange;
+        TestComputeIntRangeMask<sbyte, Int8Array, Int8Array.Builder>(rangeStart, rangeEnd, SByteValues, val => val);
+        TestComputeIntRangeMask<short, Int16Array, Int16Array.Builder>(rangeStart, rangeEnd, ShortValues, val => val);
+        TestComputeIntRangeMask<int, Int32Array, Int32Array.Builder>(rangeStart, rangeEnd, IntValues, val => val);
+        TestComputeIntRangeMask<long, Int64Array, Int64Array.Builder>(rangeStart, rangeEnd, LongValues, val => val);
+        TestComputeIntRangeMask<byte, UInt8Array, UInt8Array.Builder>(rangeStart, rangeEnd, ByteValues, val => val);
+        TestComputeIntRangeMask<ushort, UInt16Array, UInt16Array.Builder>(rangeStart, rangeEnd, UShortValues, val => val);
+        TestComputeIntRangeMask<uint, UInt32Array, UInt32Array.Builder>(rangeStart, rangeEnd, UIntValues, val => val);
+        TestComputeIntRangeMask<ulong, UInt64Array, UInt64Array.Builder>(rangeStart, rangeEnd, ULongValues, val => checked((long)val));
+    }
 
-        foreach (var (value, expected) in new[]
-                 {
-                     (1, false),
-                     (2, true),
-                     (3, true),
-                     (5, true),
-                     (6, false),
-                 })
+    private static void TestComputeIntEqualityMask<T, TArray, TBuilder>(long filterValue, T[] values, Func<T, long> checkedCast)
+        where T : struct
+        where TArray : PrimitiveArray<T>
+        where TBuilder : PrimitiveArrayBuilder<T, TArray, TBuilder>, new()
+    {
+        var filter = Col.Named("x").IsEqualTo(filterValue);
+
+        var array = BuildArray<T, TArray, TBuilder>(values);
+        var recordBatch = new RecordBatch.Builder().Append("x", true, array).Build();
+
+        var mask = filter.ComputeMask(recordBatch);
+
+        Assert.That(mask, Is.Not.Null);
+        Assert.That(mask!.Mask.Length, Is.EqualTo(BitUtility.ByteCount(array.Length)));
+
+        for (var i = 0; i < array.Length; ++i)
         {
-            TestIntColumnFilterWithAllTypes(filter, value, expected);
+            var isIncluded = BitUtility.GetBit(mask.Mask.Span, i);
+            var arrayValue = array.GetValue(i);
+            var isEqual = false;
+            if (arrayValue.HasValue)
+            {
+                try
+                {
+                    isEqual = filterValue == checkedCast(arrayValue.Value);
+                }
+                catch (OverflowException)
+                {
+                }
+            }
+
+            Assert.That(
+                isIncluded, Is.EqualTo(isEqual),
+                $"Expected {typeof(T)} value {arrayValue} inclusion to be {isEqual}");
         }
     }
 
-    [Test]
-    public void TestIntRangeFilterCrossingZero()
+    private static void TestComputeIntRangeMask<T, TArray, TBuilder>(long rangeStart, long rangeEnd, T[] values, Func<T, long> checkedCast)
+        where T : struct
+        where TArray : PrimitiveArray<T>
+        where TBuilder : PrimitiveArrayBuilder<T, TArray, TBuilder>, new()
     {
-        var filter = Col.Named("x").IsInRange(-2, 5);
+        var filter = Col.Named("x").IsInRange(rangeStart, rangeEnd);
 
-        foreach (var (value, expected) in new[]
-                 {
-                     (0, true),
-                     (1, true),
-                     (5, true),
-                     (6, false),
-                 })
-        {
-            TestIntColumnFilterWithAllTypes(filter, value, expected);
-        }
+        var array = BuildArray<T, TArray, TBuilder>(values);
+        var recordBatch = new RecordBatch.Builder().Append("x", true, array).Build();
 
-        foreach (var (value, expected) in new[]
-                 {
-                     (-3, false),
-                     (-2, true),
-                     (-1, true),
-                 })
+        var mask = filter.ComputeMask(recordBatch);
+
+        Assert.That(mask, Is.Not.Null);
+        Assert.That(mask!.Mask.Length, Is.EqualTo(BitUtility.ByteCount(array.Length)));
+
+        for (var i = 0; i < array.Length; ++i)
         {
-            TestInt8ColumnFilter(filter, (sbyte)value, expected);
-            TestInt16ColumnFilter(filter, (short)value, expected);
-            TestInt32ColumnFilter(filter, (int)value, expected);
-            TestInt64ColumnFilter(filter, (long)value, expected);
+            var isIncluded = BitUtility.GetBit(mask.Mask.Span, i);
+            var arrayValue = array.GetValue(i);
+            var isInRange = false;
+            if (arrayValue.HasValue)
+            {
+                try
+                {
+                    var asLong = checkedCast(arrayValue.Value);
+                    isInRange = asLong >= rangeStart && asLong <= rangeEnd;
+                }
+                catch (OverflowException)
+                {
+                }
+            }
+
+            Assert.That(
+                isIncluded, Is.EqualTo(isInRange),
+                $"Expected {typeof(T)} value {arrayValue} inclusion to be {isInRange}");
         }
     }
 
-    [Test]
-    public void TestNegativeIntRangeFilter()
-    {
-        var filter = Col.Named("x").IsInRange(-5, -2);
-
-        foreach (var value in new[] { 0, 1 })
-        {
-            TestIntColumnFilterWithAllTypes(filter, value, false);
-        }
-
-        foreach (var (value, expected) in new[]
-                 {
-                     (-6, false),
-                     (-5, true),
-                     (-2, true),
-                     (-1, false),
-                 })
-        {
-            TestInt8ColumnFilter(filter, (sbyte)value, expected);
-            TestInt16ColumnFilter(filter, (short)value, expected);
-            TestInt32ColumnFilter(filter, (int)value, expected);
-            TestInt64ColumnFilter(filter, (long)value, expected);
-        }
-    }
-
-    private static void TestIntColumnFilterWithAllTypes(IFilter filter, long? value, bool expected)
-    {
-        TestUInt8ColumnFilter(filter, (byte?)value, expected);
-        TestUInt16ColumnFilter(filter, (ushort?)value, expected);
-        TestUInt32ColumnFilter(filter, (uint?)value, expected);
-        TestUInt64ColumnFilter(filter, (ulong?)value, expected);
-        TestInt8ColumnFilter(filter, (sbyte?)value, expected);
-        TestInt16ColumnFilter(filter, (short?)value, expected);
-        TestInt32ColumnFilter(filter, (int?)value, expected);
-        TestInt64ColumnFilter(filter, (long?)value, expected);
-    }
-
-    private static void TestUInt8ColumnFilter(IFilter filter, byte? value, bool expected) =>
-        TestIntColumnFilter<byte, UInt8Array, UInt8Array.Builder>(filter, value, expected);
-
-    private static void TestUInt16ColumnFilter(IFilter filter, ushort? value, bool expected) =>
-        TestIntColumnFilter<ushort, UInt16Array, UInt16Array.Builder>(filter, value, expected);
-
-    private static void TestUInt32ColumnFilter(IFilter filter, uint? value, bool expected) =>
-        TestIntColumnFilter<uint, UInt32Array, UInt32Array.Builder>(filter, value, expected);
-
-    private static void TestUInt64ColumnFilter(IFilter filter, ulong? value, bool expected) =>
-        TestIntColumnFilter<ulong, UInt64Array, UInt64Array.Builder>(filter, value, expected);
-
-    private static void TestInt8ColumnFilter(IFilter filter, sbyte? value, bool expected) =>
-        TestIntColumnFilter<sbyte, Int8Array, Int8Array.Builder>(filter, value, expected);
-
-    private static void TestInt16ColumnFilter(IFilter filter, short? value, bool expected) =>
-        TestIntColumnFilter<short, Int16Array, Int16Array.Builder>(filter, value, expected);
-
-    private static void TestInt32ColumnFilter(IFilter filter, int? value, bool expected) =>
-        TestIntColumnFilter<int, Int32Array, Int32Array.Builder>(filter, value, expected);
-
-    private static void TestInt64ColumnFilter(IFilter filter, long? value, bool expected) =>
-        TestIntColumnFilter<long, Int64Array, Int64Array.Builder>(filter, value, expected);
-
-    private static void TestIntColumnFilter<T, TArray, TBuilder>(IFilter filter, T? value, bool expected)
+    private static TArray BuildArray<T, TArray, TBuilder>(T[] values)
         where T : struct
         where TArray : IArrowArray
         where TBuilder : PrimitiveArrayBuilder<T, TArray, TBuilder>, new()
     {
         var builder = new TBuilder();
-        if (value.HasValue)
-        {
-            builder.Append(value.Value);
-        }
-        else
+        foreach (var value in values)
         {
             builder.AppendNull();
+            builder.Append(value);
         }
 
-        var partitionInfo = new PartitionInformation(
-            new RecordBatch.Builder()
-                .Append("x", nullable: true, builder)
-                .Build());
-
-        Assert.That(filter.IncludePartition(partitionInfo), Is.EqualTo(expected));
+        return builder.Build();
     }
 }
