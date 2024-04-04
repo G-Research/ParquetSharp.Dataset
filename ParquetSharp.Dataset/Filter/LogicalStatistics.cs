3787f91e2e6959ffa659bb618cc97ec624f0ecd1
Adam Reeve <adam.reeve@gr-oss.io>
2024-04-04 14:32:29 +1300

    Tidy up statistics construction

--- ParquetSharp.Dataset/Filter/LogicalStatistics.cs
+++ ParquetSharp.Dataset/Filter/LogicalStatistics.cs
@@ -16,32 +16,30 @@ public abstract class LogicalStatistics
         {
             return (statistics, logicalType) switch
             {
-                (Statistics<bool> stats, NoneLogicalType) => new LogicalStatistics<bool>(stats.Min, stats.Max),
-                (Statistics<int> stats, NoneLogicalType) => new LogicalStatistics<int>(stats.Min, stats.Max),
-                (Statistics<long> stats, NoneLogicalType) => new LogicalStatistics<long>(stats.Min, stats.Max),
-                (Statistics<int> stats, IntLogicalType { BitWidth: 8, IsSigned: true }) => new LogicalStatistics<sbyte>(
-                    (sbyte)stats.Min, (sbyte)stats.Max),
-                (Statistics<int> stats, IntLogicalType { BitWidth: 8, IsSigned: false }) => new LogicalStatistics<byte>(
-                    (byte)unchecked((uint)stats.Min), (byte)unchecked((uint)stats.Max)),
-                (Statistics<int> stats, IntLogicalType { BitWidth: 16, IsSigned: true }) => new LogicalStatistics<short>(
-                    (short)stats.Min, (short)stats.Max),
-                (Statistics<int> stats, IntLogicalType { BitWidth: 16, IsSigned: false }) => new LogicalStatistics<ushort>(
-                    (ushort)unchecked((uint)stats.Min), (ushort)unchecked((uint)stats.Max)),
-                (Statistics<int> stats, IntLogicalType { BitWidth: 32, IsSigned: true }) => new LogicalStatistics<int>(
-                    stats.Min, stats.Max),
-                (Statistics<int> stats, IntLogicalType { BitWidth: 32, IsSigned: false }) => new LogicalStatistics<uint>(
-                    unchecked((uint)stats.Min), unchecked((uint)stats.Max)),
-                (Statistics<long> stats, IntLogicalType { BitWidth: 64, IsSigned: true }) => new LogicalStatistics<long>(
-                    stats.Min, stats.Max),
-                (Statistics<long> stats, IntLogicalType { BitWidth: 64, IsSigned: false }) => new LogicalStatistics<ulong>(
-                    unchecked((ulong)stats.Min), unchecked((ulong)stats.Max)),
-                (Statistics<FixedLenByteArray> stats, Float16LogicalType) => new LogicalStatistics<Half>(
-                    LogicalRead.ToHalf(stats.Min), LogicalRead.ToHalf(stats.Max)),
-                (Statistics<float> stats, NoneLogicalType) => new LogicalStatistics<float>(stats.Min, stats.Max),
-                (Statistics<double> stats, NoneLogicalType) => new LogicalStatistics<double>(stats.Min, stats.Max),
+                (Statistics<bool> stats, NoneLogicalType) => CreateStatistics<bool, bool>(stats, val => val),
+                (Statistics<int> stats, NoneLogicalType) => CreateStatistics<int, int>(stats, val => val),
+                (Statistics<long> stats, NoneLogicalType) => CreateStatistics<long, long>(stats, val => val),
+                (Statistics<int> stats, IntLogicalType { BitWidth: 8, IsSigned: true }) => CreateStatistics<int, sbyte>(stats, val => (sbyte)val),
+                (Statistics<int> stats, IntLogicalType { BitWidth: 8, IsSigned: false }) => CreateStatistics<int, byte>(stats, val => (byte)unchecked((uint)val)),
+                (Statistics<int> stats, IntLogicalType { BitWidth: 16, IsSigned: true }) => CreateStatistics<int, short>(stats, val => (short)val),
+                (Statistics<int> stats, IntLogicalType { BitWidth: 16, IsSigned: false }) => CreateStatistics<int, ushort>(stats, val => (ushort)unchecked((uint)val)),
+                (Statistics<int> stats, IntLogicalType { BitWidth: 32, IsSigned: true }) => CreateStatistics<int, int>(stats, val => val),
+                (Statistics<int> stats, IntLogicalType { BitWidth: 32, IsSigned: false }) => CreateStatistics<int, uint>(stats, val => unchecked((uint)val)),
+                (Statistics<long> stats, IntLogicalType { BitWidth: 64, IsSigned: true }) => CreateStatistics<long, long>(stats, val => val),
+                (Statistics<long> stats, IntLogicalType { BitWidth: 64, IsSigned: false }) => CreateStatistics<long, ulong>(stats, val => unchecked((ulong)val)),
+                (Statistics<FixedLenByteArray> stats, Float16LogicalType) => CreateStatistics<FixedLenByteArray, Half>(stats, LogicalRead.ToHalf),
+                (Statistics<float> stats, NoneLogicalType) => CreateStatistics<float, float>(stats, val => val),
+                (Statistics<double> stats, NoneLogicalType) => CreateStatistics<double, double>(stats, val => val),
                 _ => null,
             };
         }
+    }
+
+    private static LogicalStatistics<TStats> CreateStatistics<TPhysical, TStats>(
+        Statistics<TPhysical> statistics, Func<TPhysical, TStats> converter)
+        where TPhysical : unmanaged
+    {
+        return new LogicalStatistics<TStats>(converter(statistics.Min), converter(statistics.Max));
     }
 
     public abstract TOut Accept<TOut>(ILogicalStatisticsVisitor<TOut> visitor);
